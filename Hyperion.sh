9b26c8364635d0683d8ec328a8b119cd99199b1e
Nicolas De Vos <nndevos@gmail.com>
2016-06-27 10:36:06 +0200

    launch properly hyperiond

--- Hyperion.sh
+++ Hyperion.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
+# Add the line below in visudo
+# $USER ALL = NOPASSWD:/bin/systemctl start hyperion.service, /bin/systemctl stop hyperion.service,
+
 path=/opt/hyperion
 x11=$path/bin/hyperion-x11
 hyperiond=$path/bin/hyperiond
 
 
 function start_fct() {
-      pidof hyperiond || `$hyperiond $path/config/hyperion.config.json > /dev/null 2>&1 &`
-      pidof hyperion-x11 || `$x11 --crop-right 1440 --skip-reply > /dev/null 2>&1 &`
+    sudo systemctl start hyperion.service
+    pidof hyperion-x11 || DISPLAY=:0.0 $x11 --crop-right 1440 --skip-reply > /dev/null 2>&1
 }
 
 function stop_fct() {
     `$path/bin/hyperion-remote -c black > /dev/null 2>&1 &` && sleep 1s
-    pidof hyperiond && killall $hyperiond 
+    sudo systemctl stop hyperion.service
     pidof hyperion-x11 && killall $x11
 }
 
